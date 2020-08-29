44741653261f2233cda13f3984c5686f37afcd1e
Carson <aaatao66@gmail.com>
2020-08-29 17:04:30 +0800

    手动ack与自动ack

---
+++ src/test/java/com/carson/rabbitmqdemo/RabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carson.rabbitmqdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
